e33f4ee0d59dae82bea61cacddb993791fdf6cba
Joey Lim <jyorin0206@gmail.com>
2022-10-07 17:11:44 +0800

    implemented goal screen

--- app/src/main/java/com/example/smaicle/GoalActivity.kt
+++ app/src/main/java/com/example/smaicle/GoalActivity.kt
@@ -3,14 +3,49 @@ package com.example.smaicle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import com.example.smaicle.adapters.GoalAdapter
+import com.example.smaicle.data.Goal
+import com.example.smaicle.databinding.ActivityGoalBinding
 
 class GoalActivity : AppCompatActivity() {
+    private val goals = listOf(
+        Goal(
+            "Recycle 5 paper items",
+            5,
+            4,
+            5
+        ),
+        Goal(
+            "Recycle 5 plastic items",
+            5,
+            4,
+            5
+        ),
+        Goal(
+            "Recycle 5 plastic bottle caps",
+            3,
+            2,
+            3
+        ),
+        Goal(
+            "Recycle 5 plastic bottles",
+            3,
+            2,
+            3
+        ),
+    )
+    private lateinit var binding: ActivityGoalBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_goal)
+        binding = ActivityGoalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.title = "Goals"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.goallist.adapter = GoalAdapter()
+        (binding.goallist.adapter as GoalAdapter).submitList(goals)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         finish()
         return super.onOptionsItemSelected(item)
---
+++ app/src/main/java/com/example/smaicle/adapters/GoalAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.smaicle.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.smaicle.R
+import com.example.smaicle.data.Goal
+import com.google.android.material.progressindicator.LinearProgressIndicator
+
+class GoalAdapter : ListAdapter<Goal, GoalVH>(GoalComparator()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GoalVH {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.goal_item_layout, parent, false)
+        return GoalVH(view)
+    }
+
+    override fun onBindViewHolder(holder: GoalVH, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class GoalVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(goal: Goal) {
+        val nameView : TextView = itemView.findViewById(R.id.goal_name)
+        val progressView : LinearProgressIndicator = itemView.findViewById(R.id.goal_progress)
+        val progressTextView : TextView = itemView.findViewById(R.id.goal_progress_name)
+        val pointsView : TextView = itemView.findViewById(R.id.goal_points)
+
+        nameView.text = goal.title
+        val progressText = "${goal.currentProgress} / ${goal.totalProgress}"
+        progressTextView.text = progressText
+        pointsView.text = "${goal.points} POINTS"
+
+        val progressPercentage = goal.currentProgress / goal.totalProgress * 100
+        progressView.progress = progressPercentage
+
+    }
+}
+
+class GoalComparator : DiffUtil.ItemCallback<Goal>() {
+    override fun areItemsTheSame(oldItem: Goal, newItem: Goal): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Goal, newItem: Goal): Boolean {
+        return oldItem.title == newItem.title
+    }
+
+}
